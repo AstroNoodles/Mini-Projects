3f355d8c96ec481cea8df811fd3c74f8f4f419ef
Michael Batavia <astroelementalmod@gmail.com>
2019-06-29 19:06:24 -0400

    Package info for distributing the AWS Lambda

---
+++ ChemistryFacts/package.js
@@ -0,0 +1,20 @@
+{
+  "name": "skill-sample-nodejs-fact-i18n",
+  "version": "2.0.0",
+  "description": "Mike's tweaked version of Amazon's fact skill.",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "alexa",
+    "skill",
+    "fact",
+    "chemistry"
+  ],
+  "author": "Mike Batavia",
+  "license": "Apache-2.0",
+  "dependencies": {
+    "ask-sdk": "^2.0.0"
+  }
+}
